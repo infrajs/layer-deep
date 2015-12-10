8fadace953023ea1e43c8909ade087307e38e818
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-10 15:59:09 +0400

    ns vendor/infrajs/controller/tests/start.php

--- Deep.php
+++ Deep.php
@@ -2,12 +2,12 @@
 
 //deep:(number),//Для crumb определяет на каком уровне от текущего будет тру... пропускает родителей. Только когда что-то будет на нужном уровне от указанного crumb
 namespace infrajs\controller\ext;
-
+use infrajs\event\Event;
 class deep
 {
 	public function init()
 	{
-		Controller::isAdd('check', function ($layer) {
+		Event::handler('layer.ischeck', function ($layer) {
 			$deep = (int) $layer['deep'];
 			if (!$deep) {
 				return;
