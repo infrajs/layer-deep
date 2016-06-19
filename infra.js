cf066e251bdab9dfeb78ddaf6fa52300b9a3cbc9
Anton <a.kiyatkin@itlf.ru>
2016-06-19 15:53:49 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,10 +1,13 @@
 //deep:(number),//Для crumb определяет на каком уровне от текущего будет тру... пропускает родителей. Только когда что-то будет на нужном уровне от указанного crumb
-Event.handler('layer.ischeck', function(layer) {
-	var deep=layer.deep||0;
-	var crumb=layer.crumb;
-	while(deep&&crumb.child){
+Event.handler('layer.ischeck', function (layer) {
+	var deep = layer.deep || 0;
+	var crumb = layer.crumb;
+	while (deep && crumb.child) {
 		deep--;
-		crumb=crumb.child;
+		crumb = crumb.child;
 	}
-	if(deep)return false;
-});
+	if (deep) {
+		layer.is_save_branch = false;
+		return false;
+	}
+}, 'deep:div');
--- infra.php
+++ infra.php
@@ -1,15 +1,5 @@
 <?php
-namespace infrajs\crumb;
-use infrajs\controller\Controller;
-use infrajs\each\Each;
-use infrajs\controller\Run;
-use infrajs\controller\Layer;
-use infrajs\path\Path;
 use infrajs\event\Event;
-use infrajs\sequence\Sequence;
-use infrajs\template\Template;
-use infrajs\controller\External;
-use infrajs\config\Config;
 
 Event::handler('layer.ischeck', function ($layer) {
 	$deep = (int) $layer['deep'];
@@ -20,6 +10,7 @@ Event::handler('layer.ischeck', function ($layer) {
 		$state = &$state->child;
 	}
 	if ($deep) {
+		$layer['is_save_branch'] = false;
 		return false;
 	}
-});
+}, 'deep:div');
